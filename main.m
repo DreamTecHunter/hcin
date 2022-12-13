bbce2de64c0dd10654bc07eb0d376d37d0ebff80
Badrillox <micperktold@tsn.at>
2022-12-13 16:04:18 +0100

    committing nonsense lol

--- main.m
+++ main.m
@@ -48,7 +48,7 @@ end
 
 
 
-
+<
 % eof
 
 clear
@@ -62,12 +62,37 @@ a = audioDatastore('wav_rec', 'includesubfolders', true);
 trainDatastoreCount = countEachLabel(aTrain);
 testDatastoreCount = countEachLabel(aTest);
 
+
+reset(aTrain)
+
 % feature extraction
 disp('(2) feature extraction')
 l_win = 0.03;
 l_ol = 0.0025;
 pov_thr = -40;
 rcr_thr = 1000;
+
+features = [];
+labels = [];
+energyThreshold = 0.005;
+zcrThreshold = 0.2;
+
+keepLen = round(length(aTrain));
+
+while hasdata(aTrain)
+    [audioIn, dsInfo] = read(adsTrain);
+
+    audioIn = audioIn(1:keepLen);
+    feat = extract(aTest, audioIn);
+    isSpeech = feat(:, featureMap.shortTimeEnergy) > energyThreshold;
+    isVoiced = feat(:, featureMap.zerocrossrate) < zcrThreshold;
+    voicedSpeech = isSpeech & isVoiced;
+    feat(~voicedSpeech, :) = [];
+    feat(:, [featureMap.zerocrossrate, featureMap.shorTimeEnergy]) = [];
+    label = repelem(dsInfro.Label, size(feat, 1));
+    features = [features;feat];
+    labels = [labels, label];
+end
 
 
 
